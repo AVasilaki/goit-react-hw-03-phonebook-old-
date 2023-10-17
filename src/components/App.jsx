a5a4ea993eaf0c9f408b5185ecc97aee341d5c59
AVasilaki <poseydon463@gmail.com>
2023-10-17 10:59:38 +0300

    copy previus

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { AddContact } from './AddContact/AddContact';
+import { Contacts } from './Contacts/Contacts';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  handleFilter = evt => {
+    this.setState({ filter: evt.target.value });
+  };
+  handleAddContact = newContact => {
+    const { contacts } = this.state;
+    if (contacts.find(contact => contact.name === newContact.name)) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { ...newContact, id: nanoid() }],
+    }));
+  };
+  onRemoveContact = id => {
+    const { contacts } = this.state;
+    this.setState({ contacts: contacts.filter(contact => contact.id !== id) });
+  };
+  render() {
+    const { filter } = this.state;
+    const { contacts } = this.state;
+    const filtredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().trim().includes(filter.toLowerCase().trim())
+    );
+    return (
+      <>
+        <AddContact addNewContact={this.handleAddContact}></AddContact>
+        <Filter handleFilter={this.handleFilter}></Filter>
+        <Contacts contacts={filtredContacts} onRemoveContact={this.onRemoveContact}></Contacts>
+      </>
+    );
+  }
+}
